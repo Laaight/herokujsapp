9a197e4fbd2c9bb81dd3aee90325616c5276de6b
mhuston <mhuston@tccm.org>
2023-01-07 22:42:46 -0700

    eh

--- App.js
+++ App.js
@@ -4,9 +4,5 @@ const app = express()
 
 app.use(express.static("client"))
 
-app.get("/", function(req,res){
-    res.send("<h1> Hello</h1>")
-})
-
 app.listen(process.env.PORT || 3001,
     () => console.log("Server is running at http://localhost:3001"));
